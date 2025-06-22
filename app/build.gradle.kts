cac14faa37c3e684712f04b1e42c0763e9490a21
rubens <rubens.s.gomes@gmail.com>
2025-06-22 18:30:13 -0500

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -178,9 +178,11 @@ spotless {
 // https://github.com/researchgate/gradle-release
 
 release {
-  git {
-    requireBranch.set("main")
-    pushReleaseVersionBranch.set("release")
+  release {
+    with(git) {
+      pushReleaseVersionBranch.set("release")
+      requireBranch.set("main")
+    }
   }
 }
 
